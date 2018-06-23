2f25cf3d37e34275a71f6a0f84cb5da4984fc45c
bstelly <bstelly1999@gmail.com>
2018-06-22 21:33:23 -0500

    Initial commit

---
+++ Assets/Scripts/AnimationBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Animation
+{
+
+    public class AnimationBehaviour : MonoBehaviour
+    {
+
+        //private bool isRunning;
+        //private bool isWalking;
+        //private bool isJumping;
+        //public Animator animator;
+        //public InputBehaviour player;
+
+        //void Start()
+        //{
+        //    animator.GetComponent<Animator>();
+        //    isRunning = player.isRunning;
+        //    isWalking = player.isWalking;
+        //    isJumping = player.isJumping;
+        //    Debug.Log("Start");
+        //}
+
+        //public void Update()
+        //{
+        //    animator.SetBool("isWalking", isWalking);
+        //    Debug.Log("current value" + player.isWalking.ToString());
+        //    animator.SetBool("isRunning", isRunning);
+        //    animator.SetBool("isJumping", isJumping);
+        //}
+
+    }
+}
---
+++ Assets/Scripts/InputBehaviour.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Animation
+{
+
+    public class InputBehaviour : MonoBehaviour
+    {
+
+        private SpriteRenderer renderer;
+        public Animator animator;
+
+        // Use this for initialization
+        void Start()
+        {
+            renderer = GetComponent<SpriteRenderer>();
+            animator = GetComponent<Animator>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                renderer.flipX = false;
+                if (Input.GetKey(KeyCode.LeftShift))
+                {
+                    transform.position += Vector3.right * 2;
+                    animator.SetBool("isRunning", true);
+                    animator.SetBool("isWalking", false);
+                }
+                else
+                {
+                    transform.position += Vector3.right;
+                    animator.SetBool("isWalking", true);
+                    animator.SetBool("isRunning", false);
+                }
+            }
+            else
+            {
+                animator.SetBool("isRunning", false);
+                animator.SetBool("isWalking", false);
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                renderer.flipX = true;
+                if (Input.GetKey(KeyCode.LeftShift))
+                {
+                    transform.position += Vector3.left * 2;
+                    animator.SetBool("isRunning", true);
+                    animator.SetBool("isWalking", false);
+                }
+                else
+                {
+                    transform.position += Vector3.left;
+                    animator.SetBool("isWalking", true);
+                    animator.SetBool("isRunning", false);
+                }
+            }
+            else
+            {
+                animator.SetBool("isRunning", false);
+                animator.SetBool("isWalking", false);
+            }
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                transform.position += Vector3.up;
+                animator.SetBool("isJumping", true);
+            }
+        }
+
+        void OnCollisionEnter2D()
+        {
+            animator.SetBool("isJumping", false);
+            Debug.Log("Hit");
+        }
+    }
+}
